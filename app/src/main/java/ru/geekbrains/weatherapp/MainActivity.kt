0098d8af39c26dca2f77cb163d7b2a9361e3ab60
Alecsey Efimov <alicsey2000@gmail.com>
2022-10-14 07:19:43 +0300

    Добавление ClickListener-а на кнопку с вызовом всплывающего уведомления

--- app/src/main/java/ru/geekbrains/weatherapp/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package ru.geekbrains.weatherapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainBtn = findViewById<AppCompatButton>(R.id.btn)
+        mainBtn.setOnClickListener {
+            Toast.makeText(it.context, "Button!!!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
