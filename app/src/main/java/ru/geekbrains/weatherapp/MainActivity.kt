9e2b41033c0aba93b3a2cf5a2ded399da3becd03
Alecsey Efimov <alicsey2000@gmail.com>
2022-10-14 06:42:39 +0300

    Перевод MainActivity на kotlin

---
+++ app/src/main/java/ru/geekbrains/weatherapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.geekbrains.weatherapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
