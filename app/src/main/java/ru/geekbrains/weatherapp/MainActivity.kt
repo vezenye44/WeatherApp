7fbfa7b9064028b7e1ef0655614b6c1d8c90c03f
Alecsey Efimov <alicsey2000@gmail.com>
2022-10-14 08:35:09 +0300

    Изменение главной активити приложения | Создание первого обьекта Date-класса Note и его копирование в новую переменную с изменением содержания. Обработка нажатий на кнопки макета | Вывод заметок на экран приложения.

--- app/src/main/java/ru/geekbrains/weatherapp/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.geekbrains.weatherapp
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
@@ -11,9 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mainBtn = findViewById<AppCompatButton>(R.id.btn)
+        val mainBtn = findViewById<AppCompatButton>(R.id.btn_toast)
         mainBtn.setOnClickListener {
             Toast.makeText(it.context, "Button!!!", Toast.LENGTH_SHORT).show()
+        }
+
+        /** Первая заметка */
+        val firstNote = Note(title = "Новая заметка", text = "Текст первой заметки")
+        /** Копия первой заметки с изменененным содержанием */
+        val secondNote = firstNote.copy(text = "Текст второй заметки")
+
+        // Вывод на экран первой заметки с помощью AlertDialog
+        val firstNoteBtn = findViewById<AppCompatButton>(R.id.btn_first_note)
+        firstNoteBtn.setOnClickListener {
+            val dialogBuilder = AlertDialog.Builder(it.context)
+            dialogBuilder.setTitle(firstNote.title).setMessage(firstNote.text).show()
+        }
+
+        // Вывод на экран второй заметки
+        val secondNoteBtn = findViewById<AppCompatButton>(R.id.btn_second_note)
+        secondNoteBtn.setOnClickListener {
+            val dialogBuilder = AlertDialog.Builder(it.context)
+            dialogBuilder.setTitle(secondNote.title).setMessage(secondNote.text).show()
         }
     }
 }
