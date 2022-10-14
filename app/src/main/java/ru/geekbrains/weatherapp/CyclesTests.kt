0bdf38b8ed48f2186f8ab12ae253a50e74b27f1e
Alecsey Efimov <alicsey2000@gmail.com>
2022-10-14 08:49:39 +0300

    Создание kotlin-файла с main-функцией для тестирования циклов Создание enum-класса для дней недели

---
+++ app/src/main/java/ru/geekbrains/weatherapp/CyclesTests.kt
@@ -0,0 +1,11 @@
+package ru.geekbrains.weatherapp
+
+import android.util.Log
+
+fun main() {
+    Log.d("@@@", "________")
+
+
+
+    Log.d("@@@", "--------")
+}
---
+++ app/src/main/java/ru/geekbrains/weatherapp/Weekday.kt
@@ -0,0 +1,5 @@
+package ru.geekbrains.weatherapp
+
+enum class Weekday {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
